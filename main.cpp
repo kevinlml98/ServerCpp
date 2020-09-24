e6d6faed7379753ee0ab0fc55e39da027fcb460c
kevinlml <kevinlml@protonmail.com>
2020-09-24 13:20:48 -0600

    Segundo commit Se crea el código que maneja el servidor con el socket

--- main.cpp
+++ main.cpp
@@ -1,6 +1,82 @@
 #include <iostream>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <string>
+#define PORT 8080
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+using namespace std;
+
+int main(){
+
+    // Create a socket
+    int listening = socket(AF_INET, SOCK_STREAM, 0);
+    if (listening == -1)
+    {
+        cerr << "Can't create a socket! Quitting" << endl;
+        return -1;
+    }
+
+
+    // Bind the ip address and port to a socket
+    sockaddr_in hint;
+    hint.sin_family = AF_INET;
+    hint.sin_port = htons( PORT );
+    inet_pton(AF_INET, "0.0.0.0", &hint.sin_addr);
+
+    bind(listening, (sockaddr*)&hint, sizeof(hint));
+
+    // Tell Winsock the socket is for listening
+    listen(listening, SOMAXCONN);
+
+    // Wait for a connection
+    sockaddr_in client;
+    socklen_t clientSize = sizeof(client);
+
+    while(true) {
+        int clientSocket = accept(listening, (sockaddr *) &client, &clientSize);
+
+        char host[NI_MAXHOST];      // Client's remote name
+        char service[NI_MAXSERV];   // Service (i.e. port) the client is connect on
+
+
+        if (getnameinfo((sockaddr *) &client, sizeof(client), host, NI_MAXHOST, service, NI_MAXSERV, 0) == 0) {
+            cout << host << " connected on port " << service << endl;
+        } else {
+            inet_ntop(AF_INET, &client.sin_addr, host, NI_MAXHOST);
+            cout << host << " connected on port " << ntohs(client.sin_port) << endl;
+        }
+
+        // While loop: accept and echo message back to client
+        char buf[4096];
+
+        while (true) {
+
+
+            // Wait for client to send data
+            int bytesReceived = recv(clientSocket, buf, 4096, 0);
+
+            if (bytesReceived == -1) {
+                cerr << "Error in recv(). Quitting" << endl;
+                break;
+            }
+
+            if (bytesReceived == 0) {
+                cout << "Client disconnected " << endl;
+                break;
+            }
+
+            cout << string(buf, 0, bytesReceived) << endl;
+
+            // Echo message back to client
+            send(clientSocket, buf, bytesReceived, 0);
+
+        }
+
+        // Close the socket
+        close(clientSocket);
+    }
     return 0;
 }
